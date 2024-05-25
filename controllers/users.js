439dd215913a1aa9084e6b1fa0f3dd6f9075bdec
Angelpod <149606603+Angelpod@users.noreply.github.com>
2024-05-25 18:15:11 +0300

    sendusersUpaded

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ const sendUserCreated = (req, res) => {
 };
 const sendUserUpdated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end({ message: "Пользователь обновлен" });
+  res.send({ message: "Пользователь обновлен" });
 };
 const sendUserDeleted = (req, res) => {
   res.setHeader("Content-Type", "application/json");
